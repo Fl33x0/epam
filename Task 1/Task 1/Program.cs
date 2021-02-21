65d0947b5ab96318322f24c33eab7ab2cccedc5d
Fl3x0 <oleg.flexo@gmail.com>
2021-02-21 21:17:24 +0400

    Task 1.1.1

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину");
+            int length = inp();
+
+            Console.WriteLine("Введите ширину");
+            int width = inp();
+
+            int s = length * width;
+
+            Console.WriteLine("Площадь = " + s);
+            Console.ReadKey();
+        }
+
+        private static int inp()
+        {
+            for (; ; )
+            {
+                int input;
+                bool a = Int32.TryParse(Console.ReadLine(), out input);
+                if (a == true)
+                {
+                    if (input <= 0) Console.WriteLine("Ошибка: введите положительное значение");
+                    else return input;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    
+    }
+}

7515e1b2cf816e53a8d26d4264f5d45c758986ce
Fl33x0 <oleg-voda@mail.ru>
2021-03-06 00:35:26 +0400

    1.2.3

---
+++ Task 1/Task 1.2/Task 1.2.3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task_1._2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите строку: ");
+            string input = Console.ReadLine();
+
+            char[] inputChars = input.ToCharArray();
+            char[] separators = new char[inputChars.Length];
+
+            for (int i = 0; i < inputChars.Length; i++)
+            {
+                if (Char.IsSeparator(inputChars[i]) | Char.IsPunctuation(inputChars[i]) == true)
+                {
+                    separators[i] = inputChars[i];
+                }
+            }
+
+            string[] words = input.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            int sum = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (Char.IsUpper(words[i][0]) == false)
+                {
+                    sum++;
+                }
+
+            }
+
+            Console.WriteLine("Число слов, начинающихся с маленькой буквы: " + sum);
+        }
+
+    }
+}
---
+++ Task 1/Task 1.2/Task 1.2.4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Task_1._2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string input = Console.ReadLine();
+
+            //char[] inputChars = input.ToCharArray();
+            char[] separators = { '?', '.', '!' };
+
+            string[] sentences = input.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder str = new StringBuilder();
+
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                str.Append(sentences[i]);
+                str.Replace(sentences[i][0], Char.ToUpper(sentences[i][0]));
+            }
+
+            Console.WriteLine(str);
+        }
+    }
+}
+

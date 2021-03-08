a01e2a4d7472e517d019e7c6c6740228b7177350
Fl33x0 <oleg-voda@mail.ru>
2021-03-08 15:43:54 +0400

    1.2.4

--- Task 1/Task 1.2/Task 1.2.4/Program.cs
+++ Task 1/Task 1.2/Task 1.2.4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
 
 namespace Task_1._2._4
 {
@@ -15,15 +16,47 @@ namespace Task_1._2._4
 
             string[] sentences = input.Split(separators, StringSplitOptions.RemoveEmptyEntries);
 
-            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                sentences[i] = sentences[i].Trim();
+            }
+
+            string[] outSentences = new string[sentences.Length];
 
             for (int i = 0; i < sentences.Length; i++)
             {
+                StringBuilder str = new StringBuilder();
                 str.Append(sentences[i]);
-                str.Replace(sentences[i][0], Char.ToUpper(sentences[i][0]));
+                str.Insert(0, Char.ToUpper(sentences[i][0]));
+                str.Remove(1, 1);
+                
+                outSentences[i] = str.ToString();
+                //str.Replace(sentences[i][0], Char.ToUpper(sentences[i][0]));
+            }           
+
+            char[] inputChars = input.ToCharArray();
+          
+            List<char> separatorsInInput = new List<char>();
+            
+            for (int i = 0; i < inputChars.Length; i++)
+            {
+                if ((inputChars[i] == '?' | inputChars[i] == '.' | inputChars[i] == '!') == true)
+                {
+                    separatorsInInput.Add(inputChars[i]);                        
+                }
+
             }
 
-            Console.WriteLine(str);
+            if (separatorsInInput.Count < outSentences.Length)
+            {
+                Console.WriteLine("Введено предложение без знака препинания в конце. Перезапустите программу и введите со знаком");
+                Environment.Exit(0);
+            }                    
+           
+            for (int i = 0; i < outSentences.Length; i++)
+            {
+                Console.Write(outSentences[i] + separatorsInInput[i] + " ");
+            }
         }
     }
 }

da5a4f86c8bfbaced83d8258cbf1a383de0f8be6
Fl33x0 <oleg-voda@mail.ru>
2021-03-18 18:08:41 +0400

    First try to Task 2

---
+++ Task 2/2.1/Task 2.1.1/MyStringClass/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace MyStringClass
+{
+    public class MyString
+    {
+        private char[] charMass;
+
+        public char[] CharMass { get; set; }
+
+        public MyString()
+        {
+            charMass = new char[16];
+        }
+
+        public MyString(string text)
+        {
+            charMass = new char[text.Length];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                charMass[i] = text[i];
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char ch in charMass)
+            {
+                sb.Append(ch);
+            }
+            return sb.ToString();
+        }
+
+        public bool Equals(char[] a, char[] b)
+        {
+            if (a.Length != b.Length)
+            {
+                return false;
+            }
+           
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Task 2/2.1/Task 2.1.1/Task 2.1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyStringClass;
+
+namespace Task_2._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyString newString = new MyString(Console.ReadLine());
+
+            Console.WriteLine(newString.ToString());
+        }
+    }
+}

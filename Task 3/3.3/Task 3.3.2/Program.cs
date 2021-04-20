47b4c8f352616d8f6356bea19762ab5f9fdee6b4
Fl33x0 <oleg-voda@mail.ru>
2021-04-20 10:07:27 +0400

    Task 3.3.2 done

---
+++ Task 3/3.3/Task 3.3.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Task_3._3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите строку для проверки: ");
+                string input = Console.ReadLine();
+                SuperString.CheckLanguage(input);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Task 3/3.3/Task 3.3.2/SuperString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Task_3._3._2
+{
+    public static class SuperString
+    {
+        public static void CheckLanguage(this string input)
+        {    
+            Regex regexRussian = new Regex(@"^[А-Яё]+$", RegexOptions.IgnoreCase);
+            MatchCollection matchesRussian = regexRussian.Matches(input);
+
+            Regex regexEnglish = new Regex(@"^[a-z]+$", RegexOptions.IgnoreCase);
+            MatchCollection matchesEnglish = regexEnglish.Matches(input);
+
+            Regex regexNumbers = new Regex(@"^[0-9]+$", RegexOptions.IgnoreCase);
+            MatchCollection matchesNumbers = regexNumbers.Matches(input);
+
+            if (matchesRussian.Count > 0)
+            {
+                Console.WriteLine("Введено русское слово");
+            }
+            else if (matchesEnglish.Count > 0)
+            {
+                Console.WriteLine("Введено английское слово");
+            }
+            else if (matchesNumbers.Count > 0)
+            {
+                Console.WriteLine("Введено число");
+            }
+            else if (input == "")
+            {
+                Console.WriteLine("Введена пустая строка");
+            }
+            else
+            {
+                Console.WriteLine("Введена совокупность нескольких слов, микс символов или букв");
+            }
+        }
+    }
+}
